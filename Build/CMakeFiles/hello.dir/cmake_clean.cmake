3ff98b12074711c8318d1eb34a4196ad4f5e986e
xyt <setsna@yeah.net>
2019-10-04 12:08:25 +0800

    hello world with cmake

---
+++ Build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/helloworld.c.obj"
+  "hello.pdb"
+  "hello.exe"
+  "hello.exe.manifest"
+  "libhello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
